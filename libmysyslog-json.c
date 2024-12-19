13f416a9667fe8ef02cbfb4d4c28d6a785de5740
Karrayy05 <alecorna05@icloud.com>
2024-12-19 17:26:09 +0300

    Update and rename libmysyslog-text.c to libmysyslog-json.c

--- libmysyslog-json.c
+++ libmysyslog-json.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include "libmysyslog.h"  // Включаем заголовочный файл
 
-void log_function(const char* msg, int level, const char* process, const char* path) {
+void log_function_json(const char* msg, int level, const char* process, const char* path) {
     time_t now = time(NULL);
     FILE *file = fopen(path, "a");
     if (file) {
-        fprintf(file, "%ld %d %s %s\n", now, level, process, msg);
+        fprintf(file, "{\"timestamp\":%ld,\"log_level\":%d,\"process\":\"%s\",\"message\":\"%s\"}\n", now, level, process, msg);
         fclose(file);
     } else {
         perror("Failed to open log file");
